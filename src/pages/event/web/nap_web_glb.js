51cc49eab2a52ba05e1a02420d122d58409447e6
studiobuttermedia <bebosua05@yahoo.com.vn>
2024-09-25 21:05:11 +0700

    Redeploy the link. Accidently missed the '=' symbol

--- src/pages/event/web/nap_web_glb.js
+++ src/pages/event/web/nap_web_glb.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 
 function NapWebGlobal() {
   useEffect(() => {
-    const mobileUrl = 'hoyolab://openURL?urlhttps%3A%2F%2Fact.hoyoverse.com%2Fzzz%2Fevent%2Fe20240925reunion-qxioxj%2Findex.html%3Fgame_biz%3Dnap_global%26hyl_auth_required%3Dtrue%26hyl_hide_status_bar%3Dtrue%26hyl_landscape%3Dtrue%26hyl_presentation_style%3Dfullscreen%26s_c%3DGJKCD9LG2K%26utm_campaign%3Dweb%26utm_medium%3Dlink%26utm_source%3Dshare';
-    const MainUrl = 'https://act.hoyoverse.com/zzz/event/e20240925reunion-qxioxj/index.html?game_biz=nap_global&hyl_auth_required=true&hyl_hide_status_bar=true&hyl_landscape=true&hyl_presentation_style=fullscreen&s_c=GJKCD9LG2K&utm_campaign=web&utm_medium=link&utm_source=share';
+    const mobileUrl = 'hoyolab://openURL?url=https%3A%2F%2Fhoyo.link%2F6ekkFEAL';
+    const MainUrl = 'https://hoyo.link/6ekkFEAL';
 
     const redirect = (url, fallbackUrl, delay) => {
         try {
