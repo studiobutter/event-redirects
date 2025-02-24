e92022efcf092c43b9238ba42357e89ed02cd85e
studiobutter <bebosua05@yahoo.com.vn>
2025-02-24 23:21:33 +0700

    Add countdown timer before redirecting to game links

--- src/pages/event/sky.js
+++ src/pages/event/sky.js
@@ -7,18 +7,32 @@ function Sky() {
     useEffect(() => { 
         if (isMobile) {
           // Redirect to mobile page
-          window.location.href = 'sky://';
+            setTimeout(() => {
+            window.location.href = 'sky://';
+            }, 5000);
         } else {
           // Redirect to desktop page
-          window.location.href = 'steam://rungameid/2325290';
+          setTimeout(() => {
+            window.location.href = 'steam://rungameid/232529';
+            }, 5000);
         }
       }, []);
+    const [countdown, setCountdown] = React.useState(5);
+
+    useEffect(() => {
+      const timer = setInterval(() => {
+      setCountdown(prevCountdown => prevCountdown - 1);
+      }, 1000);
+
+      return () => clearInterval(timer);
+    }, []);
+
     return (
       <div>
-        <InAppBrowserRedirect />
-        <p>{isMobile ? "Opening Sky: Children of the Light..." : "Opening Sky: Children of the Light in Steam..."}</p>
-
-        <p>If the game fails to open or throws an invalid error, then you might not have the game. Please download the game <a href='https://www.thatskygame.com/'>here</a></p>
+      <InAppBrowserRedirect />
+      <p>{isMobile ? "Opening Sky: Children of the Light... " : "Opening Sky: Children of the Light in Steam..."}</p>
+      <p>Opening in {countdown} seconds...</p>
+      <p>If the game fails to open or throws an invalid error, then you might not have the game. Please download the game <a href='https://www.thatskygame.com/'>here</a></p>
       </div>
     );
 }
