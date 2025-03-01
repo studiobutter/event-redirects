d128fca3b1bedfbca065157a610bcc1cec6ba7e2
studiobutter <bebosua05@yahoo.com.vn>
2025-03-02 01:37:51 +0700

    Update Sky Redirects

--- src/pages/event/yu.js
+++ src/pages/event/yu.js
@@ -3,7 +3,11 @@ import { isMobile } from 'react-device-detect'
 
 import InAppBrowserRedirectZH from './components/InAppBrowserRedirect_zh-Hans';
 
-function Sky() {
+const Url = 'https://loadingbaycn.webapp.163.com/app/v1/download_client/windows/mkt-ma75-official/url'
+const Android_Url = 'https://adl.netease.com/d/g/sky/c/gw?type=android'
+const iOS_Url = 'https://adl.netease.com/d/g/sky/c/gw?type=ios'
+
+function Yu() {
     useEffect(() => { 
         if (isMobile) {
           // Redirect to mobile page
@@ -13,13 +17,22 @@ function Sky() {
           window.location.href = 'fevergames://mygame/?gameId=63';
         }
       }, []);
+      const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      let downloadUrl = Url;
+      if (/android/i.test(userAgent)) {
+        downloadUrl = Android_Url;
+      } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        downloadUrl = iOS_Url;
+      }
+
     return (
       <div>
         <InAppBrowserRedirectZH />
         <p>启动光·遇...</p>
-        <p>如果游戏无法打开或出现无效错误，则您可能没有该游戏。请从<a href='https://sky.163.com/'>此处</a>下载游戏</p>
+        <p>如果游戏无法打开或出现无效错误，则您可能没有该游戏。请从<a href={downloadUrl}>此处</a>下载游戏</p>
       </div>
     );
 }
 
-export default Sky;
+export default Yu;
