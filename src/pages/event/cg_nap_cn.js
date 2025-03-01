31948e4a092564392c9647927cd528412d413443
studiobutter <bebosua05@yahoo.com.vn>
2025-03-02 01:06:35 +0700

    Update Redirection

--- src/pages/event/cg_nap_cn.js
+++ src/pages/event/cg_nap_cn.js
@@ -1,16 +1,29 @@
 import React, { useEffect } from 'react'; // eslint-disable-line no-unused-vars
 import InAppBrowserRedirectZH from './components/InAppBrowserRedirect_zh-Hans';
 
+iOS_Url = "https://act-api-takumi.mihoyo.com/event/download_porter/link/clgm_nap-cn/official/ios_default"
+Android_Url = "https://act-api-takumi.mihoyo.com/event/download_porter/link/clgm_nap-cn/official/android_cloudgame"
+
 function CloudNAPCN() {
     useEffect(() => { 
       window.location.href = 'cloudnap://';
       }, []);
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    const downloadUrl = isIOS ? iOS_Url : Android_Url;
+
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
     return (
       <div>
-        <InAppBrowserRedirectZH />
-        <p>启动云·绝区零...</p>
-
-        <p>如果游戏无法打开或出现无效错误，则您可能没有该游戏。请在<a href='https://zzz.mihoyo.com/cloud-feat/m/'>此处</a>下载游戏.</p>
+      {isMobile ? (
+      <>
+      <InAppBrowserRedirectZH />
+      <p>启动云·绝区零...</p>
+      <p>如果游戏无法打开或出现无效错误，则您可能没有该游戏。请在<a href={downloadUrl}>此处</a>下载游戏。</p>
+      </>
+      ) : (
+      <p>不支持在PC上使用</p>
+      )}
       </div>
     );
 }
--- src/pages/event/cg_sr_cn.js
+++ src/pages/event/cg_sr_cn.js
@@ -12,12 +12,24 @@ function CloudSRCN() {
         window.location.href = 'https://sr.mihoyo.com/cloud';
       }
       }, []);
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+    const downloadUrl = isIOS ? iOS_Url : Android_Url;
+
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
     return (
       <div>
       <InAppBrowserRedirectZH />
+      {isMobile ? (
+      <>
+      <InAppBrowserRedirectZH />
       <p>启动云·星穹铁道...</p>
 
-      <p>如果游戏无法打开或出现无效错误，则您可能没有该游戏。请在<a href={/Mobi|Android/i.test(navigator.userAgent) ? Android_Url : iOS_Url}>此处</a>下载游戏.</p>
+      <p>如果游戏无法打开或出现无效错误，则您可能没有该游戏。请在<a href={downloadUrl}>此处</a>下载游戏。</p>
+      </>
+      ) : (
+      <p>重定向中...</p>
+      )}
       </div>
     );
 }
