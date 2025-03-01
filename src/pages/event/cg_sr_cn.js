d55d454cc4e76803917f3cfc46f8dc1f9ea121f5
studiobutter <bebosua05@yahoo.com.vn>
2025-03-02 00:56:54 +0700

    Updates to Redirection

--- src/pages/event/cg_sr_cn.js
+++ src/pages/event/cg_sr_cn.js
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react'; // eslint-disable-line no-unused-vars
 import InAppBrowserRedirectZH from './components/InAppBrowserRedirect_zh-Hans';
 
+iOS_Url = 'https://apps.apple.com/cn/app/id6475038985'
+Android_Url = 'https://act-api-takumi.mihoyo.com/event/download_porter/link/clgm_hkrpg-cn/official/android_default'
+
 function CloudSRCN() {
     useEffect(() => { 
-      window.location.href = 'srcloud://';
+      if (/Mobi|Android/i.test(navigator.userAgent)) {
+        window.location.href = 'srcloud://';
+      } else {
+        window.location.href = 'https://sr.mihoyo.com/cloud';
+      }
       }, []);
     return (
       <div>
-        <InAppBrowserRedirectZH />
-        <p>启动云·星穹铁道...</p>
+      <InAppBrowserRedirectZH />
+      <p>启动云·星穹铁道...</p>
 
-        <p>如果游戏无法打开或出现无效错误，则您可能没有该游戏。请在<a href='https://sr.mihoyo.com/cloud/m/#/download'>此处</a>下载游戏.</p>
+      <p>如果游戏无法打开或出现无效错误，则您可能没有该游戏。请在<a href={/Mobi|Android/i.test(navigator.userAgent) ? Android_Url : iOS_Url}>此处</a>下载游戏.</p>
       </div>
     );
 }
--- src/pages/event/cg_ys_cn.js
+++ src/pages/event/cg_ys_cn.js
@@ -1,16 +1,34 @@
 import React, { useEffect } from 'react'; // eslint-disable-line no-unused-vars
 import InAppBrowserRedirectZH from './components/InAppBrowserRedirect_zh-Hans';
 
+iOS_Url = 'https://api-takumi.mihoyo.com/event/download_porter/link/clgm_cn/official/ios_default'
+Android_Url = 'https://api-takumi.mihoyo.com/event/download_porter/link/clgm_cn/official/android_web'
+Url = 'https://api-takumi.mihoyo.com/event/download_porter/link/clgm_cn/official/pc_web'
+
 function CloudYSCN() {
     useEffect(() => { 
       window.location.href = 'yscloud://';
       }, []);
+    const getDownloadUrl = () => {
+      const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      if (/android/i.test(userAgent)) {
+      return Android_Url;
+      }
+
+      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return iOS_Url;
+      }
+
+      return Url;
+    };
+
     return (
       <div>
-        <InAppBrowserRedirectZH />
-        <p>启动云·原神...</p>
+      <InAppBrowserRedirectZH />
+      <p>启动云·原神...</p>
 
-        <p>如果游戏无法打开、出现无效错误或您尝试在 PC 上打开此游戏，则可能是您没有该游戏。请<a href='https://ys.mihoyo.com/cloud/m/#/download'>此处</a>下载游戏或在 PC 上打开云·原神</p>
+      <p>如果游戏无法打开、出现无效错误或您尝试在 PC 上打开此游戏，则可能是您没有该游戏。请<a href={getDownloadUrl()}>此处</a>下载游戏或在 PC 上打开云·原神</p>
       </div>
     );
 }
