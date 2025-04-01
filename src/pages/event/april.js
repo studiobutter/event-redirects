e4339807722588c0133a7aa2c9b1d1b1702dcb99
studiobutter <bebosua05@yahoo.com.vn>
2025-04-01 11:39:35 +0700

    Enhance April event page: delay iframe display and update YouTube URL

--- src/pages/event/april.js
+++ src/pages/event/april.js
@@ -1,21 +1,27 @@
 import React, { useEffect } from 'react'; // eslint-disable-line no-unused-vars
 
-const Url = 'https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=0';
+const Url = 'https://www.youtube.com/embed/dQw4w9WgXcQ?si=1_dtZGeKhSwWiTN8';
 
 function April() {
 
+  const [showIframe, setShowIframe] = React.useState(false);
+  const [showText, setShowText] = React.useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowIframe(true);
+      setShowText(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
-      <p>Claiming your prize...</p>
-      <iframe
-        width="0"
-        height="0"
-        style={{ display: 'none' }}
-        src={Url}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="autoplay"
-      ></iframe>
+      {showText && <p>Claiming your prize...</p>}
+      {showIframe && (
+        <iframe width="560" height="315" src={Url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      )}
     </div>
   );
 }
