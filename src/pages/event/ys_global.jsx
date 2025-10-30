926d206e9a8281b6de7c2c262f2134adf1c1612c
studiobutter <bebosua05@yahoo.com.vn>
2025-10-30 13:57:31 +0700

    Test Number 3 - Disable Instagram and Facebook redirects

--- src/pages/event/ys_global.jsx
+++ src/pages/event/ys_global.jsx
@@ -1,31 +1,43 @@
-import React, { useEffect } from 'react'; // eslint-disable-line no-unused-vars
-import { isMobile } from 'react-device-detect'
+import React, { useEffect } from "react"; // eslint-disable-line no-unused-vars
+import { isMobile } from "react-device-detect";
 
-import InAppBrowserRedirect from '../components/InAppBrowserRedirect';
+import InAppBrowserRedirect from "../components/InAppBrowserRedirect";
 
 function YSglobal() {
-    useEffect(() => { 
-        if (isMobile) {
-          // Redirect to mobile page
-          window.location.href = 'genshin://';
-        } else {
-          // Redirect to desktop page
-          window.location.href = 'hyp-global://launchgame?gamebiz=hk4e_global&openGame=true';
-        }
-      }, []);
-    return (
-      <div>
-        <InAppBrowserRedirect />
-        <p>{isMobile ? "Opening Genshin Impact..." : "Opening HoYoPlay..."}</p>
+  useEffect(() => {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    const isInstagram = /Instagram/i.test(userAgent);
+    const isFacebook = /FBAN|FBAV/i.test(userAgent);
+    const isInApp = isInstagram || isFacebook;
 
-        <p style={{ display: 'inline', marginRight: '1px' }}>If the game fails to open or throws an invalid error, then you might not have the game. Please download the game </p>
-        {isMobile ? (
-          <a href='https://hoyo.link/euHgFDAL'>here</a>
-           ) : (
-           <a href='https://sg-public-api.hoyoverse.com/event/download_porter/trace/hyp_global/hyphoyoverse/default'>here</a>
-           )}
-      </div>
-    );
+    if (!isInApp) {
+      if (isMobile) {
+        window.location.href = "genshin://";
+      } else {
+        window.location.href =
+          "hyp-global://launchgame?gamebiz=hk4e_global&openGame=true";
+      }
+    }
+  }, []);
+
+  return (
+    <div>
+      <InAppBrowserRedirect />
+      <p>{isMobile ? "Opening Genshin Impact..." : "Opening HoYoPlay..."}</p>
+
+      <p style={{ display: "inline", marginRight: "1px" }}>
+        If the game fails to open or throws an invalid error, then you might not
+        have the game. Please download the game{" "}
+      </p>
+      {isMobile ? (
+        <a href="https://hoyo.link/euHgFDAL">here</a>
+      ) : (
+        <a href="https://sg-public-api.hoyoverse.com/event/download_porter/trace/hyp_global/hyphoyoverse/default">
+          here
+        </a>
+      )}
+    </div>
+  );
 }
 
 export default YSglobal;
