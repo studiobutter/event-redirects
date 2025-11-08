bac5f34e9f4ddf1b6860c478934c974802cbc693
studiobutter <bebosua05@yahoo.com.vn>
2025-11-08 23:25:53 +0700

    Add Beta Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import AppRoutes from './Routes';
 
+import TopStripe from './pages/components/Banner'
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ paddingTop: "56px" }}>
+      <TopStripe text="⚠️ Heads up: Using Nexus Anima and Planet Petit beta launcher may render the link unusable. Opening HoYoPlay will fix the issue." />
       <AppRoutes />
     </div>
   );
---
+++ src/pages/components/Banner.jsx
@@ -0,0 +1,11 @@
+import "./Banner.css"; // Import the CSS file
+
+const TopStripe = ({ text }) => {
+  return (
+    <div className="top-stripe">
+      {text}
+    </div>
+  );
+};
+
+export default TopStripe;
