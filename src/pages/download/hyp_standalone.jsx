e6224ac7dae9c90fa3cbde5a2273d1e037e27fec
studiobutter <bebosua05@yahoo.com.vn>
2026-02-28 09:15:54 +0700

    Add more standalone link for other regions

--- src/pages/download/hyp_standalone.jsx
+++ src/pages/download/hyp_standalone.jsx
@@ -10,6 +10,10 @@ function HYPStandalone() {
     bh3_global_jp: "https://cdn.studiobutter.io.vn/hoyo_dl/bh3_global-jp.exe",
     bh3_global_kr: "https://cdn.studiobutter.io.vn/hoyo_dl/bh3_global-kr.exe",
     bh3_global_overseas: "https://cdn.studiobutter.io.vn/hoyo_dl/bh3_global-overseas.exe",
+    hk4e_global_gplay: "https://cdn.studiobutter.io.vn/hoyo_dl/hk4e_global-gplay.exe",
+    hk4e_cn_b: "https://pkg.biligame.com/games/umfgRO5gh5_1.12.0.322_14_0_bilibili_prod_202601301041_wCjtOgbg/074433/umfgRO5gh5_1.12.0.322_14_0_bilibili_prod_202601301041_wCjtOgbg.exe",
+    hkrpg_cn_b: "https://pkg.biligame.com/games/StarRail_bilibili_0130/641066/StarRail_bilibili_0130.exe",
+    nap_cn_b: "https://pkg.biligame.com/games/ZenlessZoneZero_setup_202601300327/642157/ZenlessZoneZero_setup_202601300327.exe",
   };
 
   // determine if the region query param is one of our known links
